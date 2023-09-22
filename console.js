63da503f0b5b5602a01f909e8f3091ca4b640997
dt19wszoleka <artur.wszolek.priv@gmail.com>
2023-09-22 12:15:27 +0200

    th

---
+++ console.js
@@ -0,0 +1,17 @@
+
+
+console.log("siemanko to moj kod");
+console.log(404);
+
+console.log(window);
+
+console.group('Bulbulator');
+console.log('a')
+console.log('b')
+console.log('c')
+console.groupEnd();
+
+
+let j = 'jeden';
+let 2 =
+console.table(["1", "2", "3"]);
