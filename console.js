f48020cfeedc40829904ce142b97b03f49398298
dt19wszoleka <artur.wszolek.priv@gmail.com>
2023-09-25 14:43:32 +0200

    siema 25

--- console.js
+++ console.js
@@ -1,17 +1,3 @@
-
-
-console.log("siemanko to moj kod");
-console.log(404);
-
-console.log(window);
-
-console.group('Bulbulator');
-console.log('a')
-console.log('b')
-console.log('c')
-console.groupEnd();
-
-
-let j = 'jeden';
-let 2 =
-console.table(["1", "2", "3"]);
+document.querySelector("header").style.backgroundColor="yellow";
+document.querySelector("header h1").style.fontFamily="Arial Black";
+document.querySelector("section").style.border="border-top:1px solid #CCC"
