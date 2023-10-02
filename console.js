a47ac6589ff1ed1efef8e4f6fa966d8511df08ec
dt19wszoleka <artur.wszolek.priv@gmail.com>
2023-10-02 14:41:34 +0200

    02,10,2023

--- console.js
+++ console.js
@@ -1,12 +1,44 @@
+//1
 document.querySelector("header").style.backgroundColor="yellow";
-console.log (document.querySelector("header h1").style.fontFamily="Arial Black");
+//2
+console.log
+(document.querySelector("header h1").style.fontFamily="Arial Black");
+console.log
+(document.querySelector("header h1").innerHtml = "Preambuła");
+//3
 document.querySelectorAll("section").style=" border-top-style: solid brown";
-console.log(document.querySelector("header h1").innerHtml = "Preambuła" );
+//4
+document.querySelector("header h1").innerHTML = "Preambuła";
 
 
 
-console.log(document.querySelector(".main_column h1"));
+
+document.querySelector(".main_column h1").innerText ="specjalnosc szefa";
+//5
+const mymaincolumn = document.querySelector(".main_column");
+const  myparagrph = document.createElement("p");
+myparagrph.innerHTML = "kebab na frytkach -_-";
+mymaincolumn.appendChild(myparagrph);
+//7
+const element = document.getElementById("h3").innerHTML='';
+//6
+document.querySelector("footer");
+const mydiv = document.createElement("div").style.backgroundColor="pink";
+console.log("div");
+document.createElement("P").innerHTML="&copy;";
+console.log("P");
 
 
-document.querySelector(".main_column h1").innerHTML ="specjalnosc szefa";
-document.createElement("p").innerText="kebsa na frytkach";
+//9
+document.createElement("section").style=
+    "border-top-style: dotted 2px; color: blue; margin-bottom: padding 10px; margin-top:10px; margin-left:10px; margin-right:20px; ".innerHTML='Zderzak';
+    
+
+
+
+
+
+
+
+
+
---
+++ zadania/zadanie2.js
@@ -0,0 +1,5 @@
+const guzik1 = document.querySelector
+('#change-inner-html-button');
+guzik1.addEventListener("click",function() {
+   document.querySelector("#change-inner-html-element").innerHTML(<i>Świetnie!</i>)
+});
