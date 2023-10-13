96c7683125cef23ce016760ef728907282705062
dt19wszoleka <artur.wszolek.priv@gmail.com>
2023-10-13 12:10:49 +0200

    13.10.2023

--- zadania/zadanie2.js
+++ zadania/zadanie2.js
@@ -3,3 +3,20 @@ const guzik1 = document.querySelector
 guzik1.addEventListener("click",function() {
    document.querySelector("#change-inner-html-element").innerHTML(<i>Świetnie!</i>)
 });
+function toggleText(number)
+{
+   const collapseElement = document.getElementById(`#`);
+   const arrowElement = document.getElementById(`id`);
+
+   if (collapseElement.style.display === 'none' || collapseElement.style.display === '')
+   {
+      collapseElement.style.display = 'block';
+      arrowElement.innerHTML = `&#8613;`;
+   }
+   else
+   {
+      collapseElement.style.display = 'none';
+      arrowElement.innerHTML = `&#8615;`;
+   }
+}
+</script>
